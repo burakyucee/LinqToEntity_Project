5dd80288e23f46eabbf942d4f24197bbeff7faea
BuRaK YüCe <burak_flcr@hotmail.com>
2019-12-21 13:15:51 +0300

    Add project files.

---
+++ LinqToEntity_Project/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace LinqToEntity_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDbQuery1 = new System.Windows.Forms.Button();
+            this.btnDbQuery2 = new System.Windows.Forms.Button();
+            this.btnDbQuery3 = new System.Windows.Forms.Button();
+            this.btnDbQuery4 = new System.Windows.Forms.Button();
+            this.btnDbQuery5 = new System.Windows.Forms.Button();
+            this.btnDbQuery6 = new System.Windows.Forms.Button();
+            this.btnDbQuery7 = new System.Windows.Forms.Button();
+            this.btnDbQuery8 = new System.Windows.Forms.Button();
+            this.btnDbQuery9 = new System.Windows.Forms.Button();
+            this.btnDbQuery10 = new System.Windows.Forms.Button();
+            this.btnDbQuery11 = new System.Windows.Forms.Button();
+            this.btnDbQuery12 = new System.Windows.Forms.Button();
+            this.btnDbQuery13 = new System.Windows.Forms.Button();
+            this.btnDbQuery14 = new System.Windows.Forms.Button();
+            this.btnDbQuery15 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(928, 211);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnDbQuery1
+            // 
+            this.btnDbQuery1.Location = new System.Drawing.Point(142, 261);
+            this.btnDbQuery1.Name = "btnDbQuery1";
+            this.btnDbQuery1.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery1.TabIndex = 1;
+            this.btnDbQuery1.Text = "DbQuery1";
+            this.btnDbQuery1.UseVisualStyleBackColor = true;
+            this.btnDbQuery1.Click += new System.EventHandler(this.btnDbQuery1_Click);
+            // 
+            // btnDbQuery2
+            // 
+            this.btnDbQuery2.Location = new System.Drawing.Point(274, 261);
+            this.btnDbQuery2.Name = "btnDbQuery2";
+            this.btnDbQuery2.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery2.TabIndex = 2;
+            this.btnDbQuery2.Text = "DbQuery2";
+            this.btnDbQuery2.UseVisualStyleBackColor = true;
+            this.btnDbQuery2.Click += new System.EventHandler(this.btnDbQuery2_Click);
+            // 
+            // btnDbQuery3
+            // 
+            this.btnDbQuery3.Location = new System.Drawing.Point(404, 261);
+            this.btnDbQuery3.Name = "btnDbQuery3";
+            this.btnDbQuery3.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery3.TabIndex = 3;
+            this.btnDbQuery3.Text = "DbQuery3";
+            this.btnDbQuery3.UseVisualStyleBackColor = true;
+            this.btnDbQuery3.Click += new System.EventHandler(this.btnDbQuery3_Click);
+            // 
+            // btnDbQuery4
+            // 
+            this.btnDbQuery4.Location = new System.Drawing.Point(535, 261);
+            this.btnDbQuery4.Name = "btnDbQuery4";
+            this.btnDbQuery4.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery4.TabIndex = 4;
+            this.btnDbQuery4.Text = "DbQuery4";
+            this.btnDbQuery4.UseVisualStyleBackColor = true;
+            this.btnDbQuery4.Click += new System.EventHandler(this.btnDbQuery4_Click);
+            // 
+            // btnDbQuery5
+            // 
+            this.btnDbQuery5.Location = new System.Drawing.Point(667, 261);
+            this.btnDbQuery5.Name = "btnDbQuery5";
+            this.btnDbQuery5.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery5.TabIndex = 5;
+            this.btnDbQuery5.Text = "DbQuery5";
+            this.btnDbQuery5.UseVisualStyleBackColor = true;
+            this.btnDbQuery5.Click += new System.EventHandler(this.btnDbQuery5_Click);
+            // 
+            // btnDbQuery6
+            // 
+            this.btnDbQuery6.Location = new System.Drawing.Point(142, 309);
+            this.btnDbQuery6.Name = "btnDbQuery6";
+            this.btnDbQuery6.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery6.TabIndex = 6;
+            this.btnDbQuery6.Text = "DbQuery6";
+            this.btnDbQuery6.UseVisualStyleBackColor = true;
+            this.btnDbQuery6.Click += new System.EventHandler(this.btnDbQuery6_Click);
+            // 
+            // btnDbQuery7
+            // 
+            this.btnDbQuery7.Location = new System.Drawing.Point(274, 309);
+            this.btnDbQuery7.Name = "btnDbQuery7";
+            this.btnDbQuery7.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery7.TabIndex = 7;
+            this.btnDbQuery7.Text = "DbQuery7";
+            this.btnDbQuery7.UseVisualStyleBackColor = true;
+            this.btnDbQuery7.Click += new System.EventHandler(this.btnDbQuery7_Click);
+            // 
+            // btnDbQuery8
+            // 
+            this.btnDbQuery8.Location = new System.Drawing.Point(404, 309);
+            this.btnDbQuery8.Name = "btnDbQuery8";
+            this.btnDbQuery8.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery8.TabIndex = 8;
+            this.btnDbQuery8.Text = "DbQuery8";
+            this.btnDbQuery8.UseVisualStyleBackColor = true;
+            this.btnDbQuery8.Click += new System.EventHandler(this.btnDbQuery8_Click);
+            // 
+            // btnDbQuery9
+            // 
+            this.btnDbQuery9.Location = new System.Drawing.Point(535, 309);
+            this.btnDbQuery9.Name = "btnDbQuery9";
+            this.btnDbQuery9.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery9.TabIndex = 9;
+            this.btnDbQuery9.Text = "DbQuery9";
+            this.btnDbQuery9.UseVisualStyleBackColor = true;
+            this.btnDbQuery9.Click += new System.EventHandler(this.btnDbQuery9_Click);
+            // 
+            // btnDbQuery10
+            // 
+            this.btnDbQuery10.Location = new System.Drawing.Point(667, 309);
+            this.btnDbQuery10.Name = "btnDbQuery10";
+            this.btnDbQuery10.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery10.TabIndex = 10;
+            this.btnDbQuery10.Text = "DbQuery10";
+            this.btnDbQuery10.UseVisualStyleBackColor = true;
+            this.btnDbQuery10.Click += new System.EventHandler(this.btnDbQuery10_Click);
+            // 
+            // btnDbQuery11
+            // 
+            this.btnDbQuery11.Location = new System.Drawing.Point(142, 351);
+            this.btnDbQuery11.Name = "btnDbQuery11";
+            this.btnDbQuery11.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery11.TabIndex = 11;
+            this.btnDbQuery11.Text = "DbQuery11";
+            this.btnDbQuery11.UseVisualStyleBackColor = true;
+            // 
+            // btnDbQuery12
+            // 
+            this.btnDbQuery12.Location = new System.Drawing.Point(274, 351);
+            this.btnDbQuery12.Name = "btnDbQuery12";
+            this.btnDbQuery12.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery12.TabIndex = 12;
+            this.btnDbQuery12.Text = "DbQuery12";
+            this.btnDbQuery12.UseVisualStyleBackColor = true;
+            // 
+            // btnDbQuery13
+            // 
+            this.btnDbQuery13.Location = new System.Drawing.Point(404, 351);
+            this.btnDbQuery13.Name = "btnDbQuery13";
+            this.btnDbQuery13.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery13.TabIndex = 13;
+            this.btnDbQuery13.Text = "DbQuery13";
+            this.btnDbQuery13.UseVisualStyleBackColor = true;
+            // 
+            // btnDbQuery14
+            // 
+            this.btnDbQuery14.Location = new System.Drawing.Point(535, 351);
+            this.btnDbQuery14.Name = "btnDbQuery14";
+            this.btnDbQuery14.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery14.TabIndex = 14;
+            this.btnDbQuery14.Text = "DbQuery14";
+            this.btnDbQuery14.UseVisualStyleBackColor = true;
+            // 
+            // btnDbQuery15
+            // 
+            this.btnDbQuery15.Location = new System.Drawing.Point(667, 351);
+            this.btnDbQuery15.Name = "btnDbQuery15";
+            this.btnDbQuery15.Size = new System.Drawing.Size(75, 23);
+            this.btnDbQuery15.TabIndex = 15;
+            this.btnDbQuery15.Text = "DbQuery15";
+            this.btnDbQuery15.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 450);
+            this.Controls.Add(this.btnDbQuery15);
+            this.Controls.Add(this.btnDbQuery14);
+            this.Controls.Add(this.btnDbQuery13);
+            this.Controls.Add(this.btnDbQuery12);
+            this.Controls.Add(this.btnDbQuery11);
+            this.Controls.Add(this.btnDbQuery10);
+            this.Controls.Add(this.btnDbQuery9);
+            this.Controls.Add(this.btnDbQuery8);
+            this.Controls.Add(this.btnDbQuery7);
+            this.Controls.Add(this.btnDbQuery6);
+            this.Controls.Add(this.btnDbQuery5);
+            this.Controls.Add(this.btnDbQuery4);
+            this.Controls.Add(this.btnDbQuery3);
+            this.Controls.Add(this.btnDbQuery2);
+            this.Controls.Add(this.btnDbQuery1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDbQuery1;
+        private System.Windows.Forms.Button btnDbQuery2;
+        private System.Windows.Forms.Button btnDbQuery3;
+        private System.Windows.Forms.Button btnDbQuery4;
+        private System.Windows.Forms.Button btnDbQuery5;
+        private System.Windows.Forms.Button btnDbQuery6;
+        private System.Windows.Forms.Button btnDbQuery7;
+        private System.Windows.Forms.Button btnDbQuery8;
+        private System.Windows.Forms.Button btnDbQuery9;
+        private System.Windows.Forms.Button btnDbQuery10;
+        private System.Windows.Forms.Button btnDbQuery11;
+        private System.Windows.Forms.Button btnDbQuery12;
+        private System.Windows.Forms.Button btnDbQuery13;
+        private System.Windows.Forms.Button btnDbQuery14;
+        private System.Windows.Forms.Button btnDbQuery15;
+    }
+}
+
---
+++ LinqToEntity_Project/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToEntity_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+
+        private void btnDbQuery1_Click(object sender, EventArgs e)
+        {
+            //Ürün fiyatları 50'ye eşit ve 50'den büyük olan ürünlerin Ürün Adı, Stock Mikatını, Pirim Fiyatını getirin ayrıca ürün fiyatlarına göre çoktan aza sıralayın.
+
+
+            dataGridView1.DataSource = db.Products.Where(x => x.UnitPrice >= 50).OrderByDescending(x => x.UnitPrice).Select(x => new
+            {
+                x.ProductID,
+                x.ProductName,
+                x.UnitsInStock,
+                x.UnitPrice
+            }).ToList();
+        }
+
+        private void btnDbQuery2_Click(object sender, EventArgs e)
+        {
+            //CategoryID'si 7'den küçük olanları, büyükten küçüğe sıralayınız.
+
+
+            dataGridView1.DataSource = db.Categories.Where(x => x.CategoryID <= 7).OrderByDescending(x => x.CategoryID).ToList();
+        }
+
+        private void btnDbQuery3_Click(object sender, EventArgs e)
+        {
+            //İngiltere'de oturan bayanların adi, soyadi, mesleği, ünvanı, ülkesi ve doğum tarihlerini listeleyin.
+
+            dataGridView1.DataSource = db.Employees.Where(x => x.TitleOfCourtesy == "Ms." || x.TitleOfCourtesy == "Mrs.").Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.Country,
+                x.TitleOfCourtesy,
+                BirthDate=x.BirthDate
+                
+            }).ToList();
+            
+        }
+
+        private void btnDbQuery4_Click(object sender, EventArgs e)
+        {
+            //Doğum TArihi 1930 ile 1960 arasında olup USA'de çalışanları isim, soyisimlerini listeleyin.
+
+            dataGridView1.DataSource = db.Employees.Where((x => SqlFunctions.DatePart("Year", x.BirthDate) >= 1930 && SqlFunctions.DatePart("Year", x.BirthDate) <= 1960 && x.Country == "USA")).Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+                x.BirthDate
+
+            }).ToList();
+        }
+
+        private void btnDbQuery5_Click(object sender, EventArgs e)
+        {
+            //Çalışanalrın firstname,lastname,titleofcourtesy ve age ekrana getirilsin, yaşa göre azalan şekilde sırlanasın.
+
+            dataGridView1.DataSource = db.Employees.OrderByDescending(x => SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)).Select(x => new
+            {
+                İsim = x.FirstName,
+                Soyİsim = x.LastName,
+                x.TitleOfCourtesy,
+                Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+
+
+            }).ToList();
+        }
+
+        private void btnDbQuery6_Click(object sender, EventArgs e)
+        {
+            //Her bir siparişleri birim fiyata göre listelenmesi.
+            dataGridView1.DataSource = db.Order_Details.OrderByDescending(x => x.UnitPrice).ToList();
+        }
+
+        private void btnDbQuery7_Click(object sender, EventArgs e)
+        {
+            //Kategorilerime göre toplam stok miktarı.
+            dataGridView1.DataSource = db.Products.GroupBy(x => x.Category.CategoryName).Select(y => new
+            {
+                KategoriAdı= y.Key,
+                ToplamStok = y.Sum(x => x.UnitsInStock)
+
+
+            }).ToList();
+        }
+
+        private void btnDbQuery8_Click(object sender, EventArgs e)
+        {
+            //Baş harfi "a" olup içinde "e" harfi bulunan çalışanları harf sırasına göre listeleyin.
+            dataGridView1.DataSource = db.Employees.Where(x => x.FirstName.StartsWith("a") && x.FirstName.Contains("e")).OrderByDescending(y => y.FirstName).ToList();
+        }
+
+        private void btnDbQuery9_Click(object sender, EventArgs e)
+        {
+            //Ürünler tablosundan Ürünadı, Stock Miktarı 20 ila 50 arasında olanları ve birim fiyatını getiriniz, birim fiyata göre çoktan az sıralayayınız.
+            dataGridView1.DataSource = db.Products.Where(x => x.UnitsInStock > 20 && x.UnitsInStock < 50).OrderByDescending(y => y.UnitPrice).Select(x => new
+            {
+                x.ProductName,
+                x.ProductID,
+                x.UnitsInStock,
+                x.UnitPrice
+            }).ToList();
+        }
+
+        private void btnDbQuery10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
